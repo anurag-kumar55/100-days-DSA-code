eda2ec41fff3bc8ee0f9a77c7c58908d407da0f6
anurag-kumar55 <anuragkumar082005@gmail.com>
2026-03-20 15:10:00 +0530

    Add files via upload

---
+++ DAY46/46.c
@@ -0,0 +1,19 @@
+/*
+Problem: Level Order Traversal
+
+Implement the solution for this problem.
+
+Input:
+- Input specifications
+
+Output:
+- Output specifications
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("Result\n");
+    return 0;
+}
