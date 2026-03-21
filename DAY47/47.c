28752b119c63803b67dbf1c2c1299cfa325ae44f
anurag-kumar55 <anuragkumar082005@gmail.com>
2026-03-21 23:04:22 +0530

    Add files via upload

---
+++ DAY47/47.c
@@ -0,0 +1,19 @@
+/*
+Problem: Height of Binary Tree
+
+Implement the solution for this problem.
+
+Input:
+- Input specifications
+
+Output:
+- Output specifications
+*/
+#include <stdio.h>
+#include <stdlib.h>
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("Result\n");
+    return 0;
+}
